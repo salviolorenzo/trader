963cd4ad71802065a81afe1c8629b4272baa9281
salviolorenzo <salviolorenzo@gmail.com>
2020-11-20 14:33:50 -0500

    creates basic node request structure

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const path = require('path');
 const fetch = require('node-fetch');
 const { uuid } = require('uuidv4');
 const CoinbasePro = require('coinbase-pro');
+const crypto = require('crypto');
 
 const Auth = require('./models/Auth');
+const { create } = require('domain');
 /* MIDDLEWARE */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,22 +29,78 @@ const api = 'https://api-public.sandbox.pro.coinbase.com';
 const apiURI = 'https://api.pro.coinbase.com';
 const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
 
-const authedClient = new CoinbasePro.AuthenticatedClient(
-	process.env.CLIENT_ID,
-	process.env.CLIENT_SECRET,
-	process.env.PASSPHRASE,
-	sandboxURI
-);
+/**
+ * CB-ACCESS-KEY The api key as a string.
+CB-ACCESS-SIGN The base64-encoded signature (see Signing a Message).
+CB-ACCESS-TIMESTAMP A timestamp for your request.
+CB-ACCESS-PASSPHRASE The passphrase you specified when creating the API key.
+All request bodies should have content type application/json and be valid JSON.
 
-const getCoinbaseAccounts = () => {};
 
-app.get('/get-accounts', (req, res) => {
-	console.log(process.env);
-	authedClient.getCoinbaseAccounts((res) => {
+ */
+const createRequest = async (endpoint, method = 'GET', data = null) => {
+	const body = data ? JSON.stringify(data) : null;
+	const time = await getTime();
+	const signature = generateSignature(endpoint, method, time, body);
+	const options = {
+		headers : {
+			'CB-ACCESS-KEY'        : process.env.CLIENT_ID,
+			'CB-ACCESS-SIGN'       : signature,
+			'CB-ACCESS-TIMESTAMP'  : time,
+			'CB-ACCESS-PASSPHRASE' : process.env.PASSPHRASE,
+			'CONTENT-TYPE'         : 'application/json'
+		},
+		method  : method,
+		body    : body
+	};
+
+	return await fetch(`${apiURI}/${endpoint}`, options).then((r) => r.json()).then((res) => {
 		console.log(res);
+		return res;
 	});
-});
+};
 
-app.listen(3000, () => {
-	console.log('Listening on 3000...');
-});
+const generateSignature = (endpoint, method, timestamp, body) => {
+	const secret = process.env.CLIENT_SECRET;
+
+	const requestPath = endpoint;
+
+	// const body = JSON.stringify({
+	// 	price      : '1.0',
+	// 	size       : '1.0',
+	// 	side       : 'buy',
+	// 	product_id : 'BTC-USD'
+	// });
+
+	// create the prehash string by concatenating required parts
+	let prehash = timestamp + method + requestPath;
+	if (body) {
+		prehash = prehash + body;
+	}
+
+	// decode the base64 secret
+	const key = Buffer(secret, 'base64');
+
+	// create a sha256 hmac with the secret
+	const hmac = crypto.createHmac('sha256', key);
+
+	// sign the require message with the hmac
+	// and finally base64 encode the result
+	return hmac.update(prehash).digest('base64');
+};
+
+getTime = async () => {
+	return await fetch(`${sandboxURI}/time`, {
+		method : 'GET'
+	})
+		.then((r) => r.json())
+		.then((res) => {
+			return res.epoch;
+		});
+};
+
+const getCoinbaseAccounts = async () => {
+	createRequest('/accounts', 'GET');
+};
+
+getCoinbaseAccounts();
